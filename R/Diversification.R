aaaa4afdd47f16c15f76473e40e7160b0340279c
Ross Cunning <ross.cunning@gmail.com>
2017-08-26 11:42:03 -0400

    fastOrtho analysis

---
+++ R/Diversification.R
@@ -0,0 +1,84 @@
+library(stringr)
+library(tidyverse)
+
+dat <- read.delim("ortho.table",header=F,sep="\t")
+colnames(dat) <- c("Group", "Genes", "Taxa", "IDs")
+dat5 <- filter(dat, Taxa>=1)
+dim(dat5) #23380
+
+counts <- data.frame(matrix(nrow=nrow(dat5),ncol=10))
+rownames(counts) <- dat5$Group
+colnames(counts) <- c("Pdam","Adig","Ofav","Ampl","Disc","Aipt","Nema","Hydr","Mnem","Amph")
+
+counts$Pdam <- apply(dat5,1,function(x) str_count(x[4],"Pocillopora"))
+counts$Adig <- apply(dat5,1,function(x) str_count(x[4],"Acropora"))
+counts$Ofav <- apply(dat5,1,function(x) str_count(x[4],"Orbicella"))
+counts$Ampl <- apply(dat5,1,function(x) str_count(x[4],"Amplexidiscus"))
+counts$Disc <- apply(dat5,1,function(x) str_count(x[4],"Discosoma"))
+counts$Aipt <- apply(dat5,1,function(x) str_count(x[4],"Aiptasia"))
+counts$Nema <- apply(dat5,1,function(x) str_count(x[4],"Nematostella"))
+counts$Hydr <- apply(dat5,1,function(x) str_count(x[4],"Hydra"))
+counts$Mnem <- apply(dat5,1,function(x) str_count(x[4],"Mnemiopsis"))
+counts$Amph <- apply(dat5,1,function(x) str_count(x[4],"Amphimedon"))
+
+###Groupings
+coral <- 1:3
+morph <- 4:5
+anem <- 6:7
+hyd <- 8
+out <- 9:10
+cnid <- c(coral,morph,anem,hyd)
+
+
+coralMin <- apply(counts[,coral],1,min)
+length(which(coralMin>0)) #7962 found in all corals (core coral genome)
+anemMin <- apply(counts[,anem],1,min)
+cnidMin <- apply(counts[,cnid],1,min)
+length(which(cnidMin>0)) #found in all cnidarians 2367
+noncorMax <- apply(counts[,-c(coral)],1,max)
+nonanemMax <- apply(counts[,-anem],1,max)
+length(which(coralMin>0 & noncorMax==0)) #in corals only 370
+length(which(anemMin>0 & nonanemMax==0)) #in anemones only 495
+
+noncorcnidMax <- apply(counts[,c(morph,anem,hyd)],1,max)
+length(which(coralMin>0 & noncorcnidMax==0)) #387
+nonanemcnidMax <- apply(counts[,c(morph,coral,hyd)],1,max)
+length(which(anemMin>0 & nonanemcnidMax==0)) #535
+
+coralMax <- apply(counts[,coral],1,max)
+noncorcnidMin <- apply(counts[,c(morph,anem,hyd)],1,min)
+length(which(coralMax==0 & noncorcnidMin>0)) #Lost in corals 1
+
+anemMax <- apply(counts[,anem],1,max)
+nonanemcnidMin <- apply(counts[,c(morph,coral,hyd)],1,min)
+length(which(anemMax==0 & nonanemcnidMin>0)) #Lost in anemones 5
+
+coreCoral <- counts[coralMin>0,]
+coralOnly <- counts[coralMin>0 & noncorMax==0,]
+
+###Species-specific genes
+k=1
+pdam <- counts[,k]
+nonpdam <- apply(counts[,-k],1,max)
+length(which(pdam>0 & nonpdam==0)) #560 in pdam only
+pdamOnly <- dat[which(pdam>0 & nonpdam==0),]
+write.table(pdamOnly,"Pdam_specific/Pdam_specific.txt",quote=F,row.names=F,col.names=T,sep="\t")
+
+###Diversification in pdam
+anth <- apply(counts[,c(coral,morph,anem)],1,min)
+pgens <- counts[which(anth>0),]
+divp <- c()
+for (i in 1:nrow(pgens)) {
+#	print(i)
+	g1 <- sum(pgens[i,1:3])
+	g2 <- sum(pgens[i,4:7])
+	n1 <- sum(pgens[-i,1:3])
+	n2 <- sum(pgens[-i,4:7])
+	divp[i] <- fisher.test(cbind(c(g1,g2),c(n1,n2)))$p.value
+}
+divadj <- p.adjust(divp,method="fdr")
+dir <- apply(pgens,1,function(x) mean(x[1:3])-mean(x[4:7]))
+divs <- pgens[divadj<0.01 & dir>0,]
+pdivGens <- dat[dat$Group%in%rownames(divs),]
+write.table(pdivGens,"CoralDiversified/CoralDiversified.txt",quote=F,row.names=F,col.names=T,sep="\t")
+
