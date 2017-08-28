8a304089a2cda4eb97670626094aeafba4443468
Ross Cunning <ross.cunning@gmail.com>
2017-08-28 11:10:19 -0400

    run gene density analysis

--- R/genedens.R
+++ R/genedens.R
@@ -3,6 +3,8 @@ library(tidyverse)
 # Get command line arguments
 args = commandArgs(trailingOnly=TRUE)
 
+window <- 100000
+
 # import a text file with gene positions
 # columns should be: chr, position (no end or gene name required)
 genes <- read.table(args[1])
@@ -12,16 +14,18 @@ genes <- genes[order(genes$scaffold, genes$loc), ]
 # scaffold lengths
 scafs <- read.table(args[2])
 colnames(scafs) <- c("scaffold", "length")
-scafs100k <- scafs[scafs$length > 100000, ]
+scafs100k <- scafs[scafs$length > window, ]
 
 # subset genes from >100k scafs
 geneset <- droplevels(merge(scafs100k, genes))
 
 dd <- plyr::ldply(split(geneset, f=list(geneset$scaffold)), function(scaf) {
-  scaf$int <- with(scaf, cut(loc, breaks=seq(0, plyr::round_any(max(length), 100000), 100000)))
+  scaf$int <- with(scaf, cut(loc, breaks=seq(0, plyr::round_any(max(length), window), window)))
   scaf %>% group_by(int) %>% summarise_each(funs(count=n()), loc)
 })
 
-png("hist.png", width=3, height=3, units="in", res=300)
-hist(dd$count)
+png(paste(args[3], "hist.png", sep="."), width=3, height=3, units="in", res=300)
+hist(dd$count, xlim=c(0,30))
 dev.off()
+
+save(dd, file=paste(args[3], "genedens.RData", sep="."))
--- Shell/get_genedens.sh
+++ Shell/get_genedens.sh
@@ -5,13 +5,12 @@
 # $2 = fasta.gz
 # $3 = output basename
 
-gunzip $1 | awk '$3 == "gene" {print}' | cut -f1,4 > $3.genes.txt
+tar -xOzf $1 | awk '$3 == "gene" {print}' | cut -f1,4 > $3.genes.txt
 
 # Get total length of each scaffold
-gunzip $2 | samtools faidx
-cut -f1-2 $2.fai > $3.scaffold.lengths.txt
-
-
+tar -xOzf $2 | samtools faidx -
+cut -f1-2 ./-.fai > $3.scaffold.lengths.txt
+rm ./-.fai
 
 # Run R script to calc gene density
-R --vanilla < genedens.R --args $3.genes.txt $3.scaffold.lengths.txt
+R --vanilla < ../R/genedens.R --args $3.genes.txt $3.scaffold.lengths.txt $3
